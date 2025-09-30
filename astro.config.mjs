8ed6c8990f14bfc97645db006d60be0ab0ca3779
mageshkrishna <gmageshkrishna2003@gmail.com>
2025-09-30 19:46:20 +0530

    runtime airflow config docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -47,32 +48,81 @@ export default defineConfig({
             {
               label: "Studio",
               items: [
-                { label: "Overview", link: "/workspace/studio/overview/" },
-                { label: "Launch Server", link: "/workspace/studio/launch-studio-server/" },
-                { label: "Explore & Engineer Data", link: "/workspace/studio/explore-and-engineer-data/" },
-                { label: "Workflows (Airflow DAGs)", link: "/workspace/studio/workflows/" },
-                { label: "MLFlow", link: "/workspace/studio/mlflow/" },
-                { label: "Visualisation & Apps", link: "/workspace/studio/visualisation-and-apps/" },
-                { label: "Environments", link: "/workspace/studio/environments/" },
-                { label: "Variables & Secrets", link: "/workspace/studio/variables-and-secrets/" },
-                { label: "Connections", link: "/workspace/studio/connections/" },
-                { label: "Git Settings", link: "/workspace/studio/git-settings/" },
-                { label: "Spark", link: "/workspace/studio/spark/" },
+                {
+                  label: "Overview",
+                  link: "/workspace/studio/overview/",
+                },
+                {
+                  label: "Launch Server",
+                  link: "/workspace/studio/launch-studio-server/",
+                },
+                {
+                  label: "Explore & Engineer Data",
+                  link: "/workspace/studio/explore-and-engineer-data/",
+                },
+                {
+                  label: "Workflows (Airflow DAGs)",
+                  link: "/workspace/studio/workflows/",
+                },
+                {
+                  label: "MLFlow",
+                  link: "/workspace/studio/mlflow/",
+                },
+                {
+                  label: "Visualisation & Apps",
+                  link: "/workspace/studio/visualisation-and-apps/",
+                },
+                {
+                  label: "Environments",
+                  link: "/workspace/studio/environments/",
+                },
+                {
+                  label: "Variables & Secrets",
+                  link: "/workspace/studio/variables-and-secrets/",
+                },
+                {
+                  label: "Connections",
+                  link: "/workspace/studio/connections/",
+                },
+                {
+                  label: "Git Settings",
+                  link: "/workspace/studio/git-settings/",
+                },
+                {
+                  label: "Spark",
+                  link: "/workspace/studio/spark/",
+                },
               ],
             },
+
             {
               label: "Runtimes",
               items: [
-                { label: "Overview", link: "/workspace/runtime/overview/" },
+                {
+                  label: "Overview",
+                  link: "/workspace/runtime/overview/",
+                },
               ],
             },
             {
               label: "Admin",
               items: [
-                { label: "Overview", link: "/workspace/admin/overview/" },
-                { label: "Users & Permissions", link: "/workspace/admin/users-roles-teams/" },
-                { label: "Studio Server Access", link: "/workspace/admin/manage-server-access/" },
-                { label: "Manage Environments", link: "/workspace/admin/manage-environment/" },
+                {
+                  label: "Overview",
+                  link: "/workspace/admin/overview/",
+                },
+                {
+                  label: "Users & Permissions",
+                  link: "/workspace/admin/users-roles-teams/",
+                },
+                {
+                  label: "Studio Server Access",
+                  link: "/workspace/admin/manage-server-access/",
+                },
+                {
+                  label: "Manage Environments",
+                  link: "/workspace/admin/manage-environment/",
+                },
                 {
                   label: "Runtime Settings",
                   items: [
@@ -80,16 +130,25 @@ export default defineConfig({
                     { label: "Airflow Runtime Settings", link: "/workspace/admin/runtime-settings/runtime-airflow-config/" },
                   ],
                 },
-                { label: "Manage Spark", link: "/workspace/admin/manage-spark/" },
+                {
+                  label: "Manage Spark",
+                  link: "/workspace/admin/manage-spark/",
+                },
               ],
-            }
+            },
           ],
         },
         {
           label: "Reference",
-          items: [
-            { label: "Connections and Secrets", link: "/reference/connection-and-secrets" },
-            { label: "Streamlit chatBot", link: "/reference/streamlit-chatbot" },
+          items:[
+            {
+              label:'Connections and Secrets',
+              link:"/reference/connection-and-secrets"
+            },
+           {
+              label:'Streamlit chatBot',
+              link:"/reference/streamlit-chatbot"
+            }
           ],
           autogenerate: {
             directory: "reference",
